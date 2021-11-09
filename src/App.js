ab6af12c5e1aee5961edda226ea33aac4c68dc59
saneprkl <santeri.lehtonen@gmail.com>
2021-11-09 13:54:11 +0200

    Header branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import Login from './components/LoginRegisteration/Login';
 
 export default function App() {
 
   return (
     <div className="App">
+      <Header />
       <Login />
     </div>
   );
