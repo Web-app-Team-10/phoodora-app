8e50da7a0f281d8115aa5e1ab07bba305a208023
saneprkl <santeri.lehtonen@gmail.com>
2021-11-24 19:24:27 +0200

    .

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import data from './data.json';
 export default function App() {
   const restaurants = data.restaurants;
   const uniqCity = [];
+  
     restaurants.map( unique => { 
         if (uniqCity.indexOf(unique.city) === -1) { uniqCity.push(unique.city) }
     });
