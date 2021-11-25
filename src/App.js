1a59f9d89f45acffc2f4ee3216b233ef33e58a70
saneprkl <santeri.lehtonen@gmail.com>
2021-11-25 22:15:09 +0200

    Restaurant creation complete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import Header from './components/Header/Header';
 import Login from './components/LoginRegisteration/Login';
 import RestaurantView from './components/Restaurant/RestaurantView';
+import CreateRestaurant from './components/CreateRestaurant/Create';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Frontpage from './components/Frontpage/Frontpage';
 import data from './data.json';
-
+import { v4 as uuid_v4 } from 'uuid';
 
 export default function App() {
   const [SearchTerm, setSearchTerm] = useState("");
@@ -13,7 +14,25 @@ export default function App() {
 
   let restaurants = data.restaurants;
   let manager;
-  if (SearchTerm.length > 1){
+
+  const addNewRestaurant = (name, address, city, hours, type, pricerange, image) => {
+    let newRestaurant = {
+    id: uuid_v4(),
+    name: name,
+    address: address,
+    city: city,
+    hours: hours,
+    type: type,
+    pricerange: pricerange,
+    image: image,
+    menu: [] }
+    restaurants.push(newRestaurant);    
+    restaurants.map( unique => { 
+        if (uniqCity.indexOf(unique.city) === -1) { uniqCity.push(unique.city) }
+    });
+  };
+  
+  if (SearchTerm.length > 0){
     restaurants = restaurants.filter((restaurants) => restaurants.name.toLowerCase().includes(SearchTerm.toLowerCase()))
   }
   else{
@@ -31,23 +50,12 @@ export default function App() {
     const restaurants_2 = restaurants.filter((restaurants) => restaurants.city.includes(randomCity_2));
     const randomRestaurants_1 = restaurants_1.sort(() => Math.random() - Math.random()).slice(0, 3);
     const randomRestaurants_2 = restaurants_2.sort(() => Math.random() - Math.random()).slice(0, 3);
-    
-    let output = <BrowserRouter>
-    <>
-      <Header setSearchTerm = {setSearchTerm} SearchTerm = {SearchTerm} />
-      <Routes>
-        <Route path="/" element={ <Frontpage restaurants={ restaurants } uniqCity={ uniqCity } restaurants_1={ restaurants_1 } restaurants_2={ restaurants_2 } randomCity_1={ randomCity_1 } randomCity_2={ randomCity_2 } randomRestaurants_1={ randomRestaurants_1 } randomRestaurants_2={ randomRestaurants_2 }/> }> </Route>
-        <Route path="/forms" element={ <Login /> }></Route>
-        <Route path="/restaurants/:id" element={ <RestaurantView restaurants={ restaurants } /> }></Route>
-        <Route path="/restaurants/:id/:category" element={ <RestaurantView restaurants={ restaurants } /> }></Route>
-      </Routes>
-    </>
-  </BrowserRouter>
 
   if(managerModeActive) {
-   manager = <div>Very secret manager mode</div>;
+   manager = <CreateRestaurant activateManagerMode={ activateManagerMode } addNewRestaurant={ addNewRestaurant } />;
   } else {
-    manager = <div>Unauthorized access</div>;
+    manager = <><div>Unauthorized access</div>
+            <div><button onClick={ activateManagerMode }>Click to gain access</button></div></>;
   }
   
   return ( <BrowserRouter>
@@ -58,7 +66,7 @@ export default function App() {
         <Route path="/forms" element={ <Login /> }></Route>
         <Route path="/restaurants/:id" element={ <RestaurantView restaurants={ restaurants } /> }></Route>
         <Route path="/restaurants/:id/:category" element={ <RestaurantView restaurants={ restaurants } /> }></Route>
-        <Route path="/restaurants/:id/manager" element={ manager }></Route>
+        <Route path="/manager" element={ manager }></Route>
       </Routes>
     </>
   </BrowserRouter>
---
+++ src/components/CreateRestaurant/Create.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import styles from './Create.module.css';
+
+export default function Create(props) {
+
+    const [ newName, setNewName ] = useState("");
+    const [ newAddress, setNewAddress ] = useState("");
+    const [ newCity, setNewCity ] = useState("");
+    const [ newHours, setNewHours ] = useState("");
+    const [ newType, setNewType ] = useState("");
+    const [ newPricerange, setNewPricerange ] = useState("");
+    const [ newImage, setNewImage ] = useState("");
+
+    const addNewRestaurant = () => {
+        props.addNewRestaurant(newName, newAddress, newCity, newHours, newType, newPricerange, newImage);
+    }
+
+    return (
+        <div className={ styles.container}>
+            <div className={ styles.title }>Register a restaurant and become restaurant manager</div>
+            <button className={ styles.button } onClick={ () => props.activateManagerMode() }>Go back</button>
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>Restaurant name:</div>
+                <div><input className={ styles.input } onChange={ (event) => setNewName(event.target.value = event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1)) } type="text" placeholder="Name restaurant"></input></div>
+            </div> 
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>Restaurant address:</div>
+                <div><input className={ styles.input } onChange={ (event) => setNewAddress(event.target.value = event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1)) } type="text" placeholder="Address of your restaurant"></input></div>
+            </div>
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>City of operation:</div>
+                <div><input className={ styles.input } onChange={ (event) => setNewCity(event.target.value = event.target.value.charAt(0).toUpperCase() + event.target.value.slice(1)) } type="text" placeholder="City of operation"></input></div>
+            </div>
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>Operating hours:</div>
+                <div><input className={ styles.input } onChange={ (event) => setNewHours(event.target.value) } type="text" placeholder="Hours e.g. 11:00-21:00"></input></div>
+            </div>
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>Restaurant type:</div>
+                <div><select className={ styles.select } onChange={ (event) => setNewType(event.target.value) }>
+                    <option value="Fast food">Fast food</option>
+                    <option value="Fast casual">Fast casual</option>
+                    <option value="Casual dining">Casual dining</option>
+                    <option value="Fine dining">Fine dining</option>
+                    </select></div>
+            </div>
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>Restaurant price level:</div>
+                <div><select className={ styles.select } onChange={ (event) => setNewPricerange(event.target.value) } name="pricerange">
+                    <option value="€">€</option>
+                    <option value="€€">€€</option>
+                    <option value="€€€">€€€</option>
+                    <option value="€€€€">€€€€</option>
+                    </select></div>
+            </div>
+            <div className={ styles.box }>
+                <div className={ styles.title2 }>Address of image:</div>
+                <div><input className={ styles.input } onChange={ (event) => setNewImage(event.target.value) } type="text" placeholder="Image address"></input></div>
+            </div>
+            <button className={ styles.button2 } onClick={ addNewRestaurant }>Create Restaurant</button>
+        </div>
+    )
+}
