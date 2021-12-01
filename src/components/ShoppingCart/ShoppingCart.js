2809a52ed8187f4275094e7857df14c6fdd5c3a5
AlikiKapasakalidi <t0kaal01@students.oamk.fi>
2021-12-01 14:29:27 +0200

    total price function is working, delete item function works in console

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,31 +1,52 @@
-import React  from 'react';
+import React, {useState}  from 'react';
 import styles from './ShoppingCart.module.css';
 import ShoppingCartItem from '../ShoppingCartItem/ShoppingCartItem';
+import {BsFillArrowLeftCircleFill} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 
 
 
 
 export default function ShoppingCart(props) {
+
+    let  total =0;
+
+    function updateTotal(){
+
+        for (let i=0; i < props.shoppingCart.length; i++){
+            
+            total += props.shoppingCart[i].price  ;
+        
+        }
+    }
+
+        let product;
+        
+        const deleteItem = productId => {
+            props.shoppingCart.map(find => { if (find.id == productId){ product = find } });
+            console.log(productId);
+            let index = product.map(product => { return product.id; }).indexOf(productId);
+            console.log(index);
+            props.setShoppingCart(product.splice(index, 1));
+            console.log(product);
+        }
+
+
+
     return (
 
         <div className={styles.ShoppingCartContainer}>
-            <button onClick={ () => console.log("Cart -->", props.shoppingCart) }>Print cart in console</button>
+        <Link to= '/' className={styles.linkBtn}> <button className={styles.goBackBtn}><BsFillArrowLeftCircleFill size={20}/> Go back</button></Link>
+        {/* <button onClick={ () => console.log("Cart -->", props.shoppingCart) }>Print cart in console</button>*/}
         <div className = {styles.addedItems}>
-        { props.shoppingCart.map( shoppingCart =>  <ShoppingCartItem  key={ shoppingCart.id }  {...shoppingCart} />)}
+        { props.shoppingCart.map( shoppingCart =>  <ShoppingCartItem deleteItem={deleteItem} key={ shoppingCart.id }  {...shoppingCart} />)}
 
-
-
-       {/* don't mind this line, because I saw in the RestaurantView.js and Menu.js that you declared and actually made the array 
-        work so that the products are pushed there after the icon is clicked so I guess the main question I have is 
-        how can I display each of them properly in the ShoppingCartItem, I tried but I don't know how to reference the array 
-        which was declared in the RestaurantView.js */}
-        
         <ShoppingCartItem />
         </div>
         <h2 className={styles.deliveryAddress}>Delivery address:</h2>
         <input className = { styles.deliveryInput} type="text" placeholder = "Type here"></input> 
-        <h3 className={styles.totalPrice}>Total price &#8364;</h3>
+        <h3 className={styles.totalPrice} onClick={updateTotal()}> {total} &#8364;</h3>
         <button className={styles.paymentBtn}>Proceed to payment</button>
         </div>
     )
--- src/components/ShoppingCartItem/ShoppingCartItem.js
+++ src/components/ShoppingCartItem/ShoppingCartItem.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ShoppingCartItem.module.css';
 import {GrClose}  from 'react-icons/gr';
+import {AiOutlinePlus} from 'react-icons/ai';
+import {AiOutlineMinus} from 'react-icons/ai';
 
 
 export default function ShoppingCartItem(props) {
+
+    const deleteItemClick = (productId) => {
+        props.deleteItem(productId);
+    }
+
+
+    
     return (
         
+        
         <div className = {styles.addedItems}>
+        
 
         <div className= {styles.item}>
-        <img  src={ `/images/${props.image}`} />
+        <img className ={styles.image} src={ `/images/${props.image}`} />
         <div>{props.name}</div>
-        <div>{props.price}</div>
-        <div  className = {styles.icon} ><GrClose size={ 25 }/></div>
-        </div>
         
-    </div>
+        <span>
+            <button  className={styles.quantityBtn} onClick = { () =>  props.quantity - 1}><AiOutlineMinus size={18}/> </button>
+            <span>{props.quantity}</span>
+            <button className={styles.quantityBtn} onClick={ () =>   props.quantity + 1}> <AiOutlinePlus size={18}/></button>
+        </span>
+        <div className={styles.priceContainer}>
+            <div  className = {styles.closeIcon} ><GrClose onClick={ () => deleteItemClick(props.id)} size={ 20 }/></div>
+            <div className ={styles.itemPrice}>{props.price} &#8364;</div>
+        </div>
+
+        </div>
+        </div>
+    
     )
 }
