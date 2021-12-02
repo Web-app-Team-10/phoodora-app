e27caef86a5d5a213faabc1b788d988a6b8f8e2e
saneprkl <santeri.lehtonen@gmail.com>
2021-12-02 19:17:02 +0200

    Restaurants are displayed from backend now

--- src/App.js
+++ src/App.js
@@ -7,16 +7,55 @@ import EditMenu from './components/Manager/EditMenu';
 import CreateRestaurant from './components/Manager/Create';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Frontpage from './components/Frontpage/Frontpage';
-import data from './data.json';
+/*import data from './data.json';*/
 import { v4 as uuid_v4 } from 'uuid';
+import axios from 'axios';
 
-export default function App() {
+export default function App()
+ {
   let [restaurants, setRestaurants] = useState([]);
   const [SearchTerm, setSearchTerm] = useState("");
   const [managerModeActive, activateManagerMode] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [testing, setTesting] = useState([]);
+  const [err, setError] = useState(null);
+
+  let dataTest;
+
+  /*useEffect(() => {
+    const getData = () => {
+      fetch('https://phoodora-app.herokuapp.com/').then(
+        response => { if(response.status >= 400) {
+                      throw new Error("Error");
+        } return response.json()
+      }).then(something => {
+        setTesting(something);
+        setIsLoading(false);
+      }, err => { setError(err); setIsLoading(false); }
+      ) };
+    getData();
+  }, [] )*/
+  useEffect(() => {
+    axios.get('https://phoodora-app.herokuapp.com/')
+      .then((response) => {
+        
+        setTesting(response.data);
+        response.data.map(restaurant => { restaurant.city = "kaupunni"; restaurant.menu = []})
+        response.data.map(restaurant => restaurants.push(restaurant))
+        console.log(restaurants)
+        setIsLoading(false);
+      });
+  }, []);
   
-  data.manager.map(manager => restaurants = manager.restaurants );
+/*data.manager.map(manager => restaurants = manager.restaurants )*/
+{console.log(restaurants, "Restaurants")}
+
+
+
+  
+  
+  
 
   const addNewRestaurant = (name, address, city, hours, type, pricerange, image) => {
     let newRestaurant = {
@@ -43,10 +82,6 @@ export default function App() {
   
   if (SearchTerm.length > 0){
     restaurants = restaurants.filter((restaurants) => restaurants.name.toLowerCase().includes(SearchTerm.toLowerCase()))
-  }
-  else{
-    data.manager.map(manager => restaurants = manager.restaurants );
-    
   }
   let uniqCity = [];
     restaurants.map( unique => { 
@@ -77,8 +112,23 @@ export default function App() {
       </>
   } else {
     authRoutes = <></>;
-  }
-  
+  }/*{ testing.map(restaurant => {<div> asdasd{restaurant.name}</div>}) }
+
+
+
+
+
+
+
+  */
+
+
+
+
+
+
+
+
   return ( <BrowserRouter>
     <>
       <Header setSearchTerm = {setSearchTerm} SearchTerm = {SearchTerm} activateManagerMode={ activateManagerMode }/>
